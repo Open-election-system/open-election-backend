af2a3b76b13baae49a6619b3e219b51df11232c1
Daniil Lohvinov <nsblnr@gmail.com>
2020-06-11 01:04:39 +0300

    added facades and builders schemas

---
+++ app/api/elections/core/elections_facade.py
@@ -0,0 +1,35 @@
+container = {}
+
+
+class ElectionsFacade:
+    @staticmethod
+    def get_elections(user_id):
+        elections = []
+        user_info = container.users_facade.get_user_info(user_id)
+        available_restrictions = container.restrictions_service.get_by_params(user_info)
+        for restriction in available_restrictions:
+            election = container.elections_service.get_by_params(restriction.id)
+            user_votes_count = container.vote_service.count(election.id, user_id)
+            elections.append({'election': election, 'restrictions': restriction, 'votes_count': user_votes_count})
+        return elections
+
+    @staticmethod
+    def get_election(election_id, user_id):
+        election_item = container.elections_service.get_by_id(election_id)
+        restrictions = container.restrictions_service.get_by_id(election_item.restrictions_id)
+        user_votes = container.vote_service.get_by_params(election_id, user_id)
+        options = container.options_service.get_by_election(election_id)
+        election = {'election': election_item, 'restrictions': restrictions, 'votes': user_votes, 'options': options}
+        return election
+
+    @staticmethod
+    def get_election_stats(election_id):
+        election_item = container.elections_service.get_by_id(election_id)
+        votes_count = container.vote_service.count(election_id)
+        options = container.options_service.get_by_election(election_id)
+        election = {'election': election_item, 'votes_count': votes_count, 'options': options}
+        return election
+
+    @staticmethod
+    def create_election(data):
+        return container.election_builder.build(data)
