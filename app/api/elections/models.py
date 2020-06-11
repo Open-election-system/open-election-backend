f248d4f2d1f369e2d6a1e7776b1a99d50fb0a1a4
scrubele <mankoania@gmail.com>
2020-06-11 18:18:25 +0300

    merge

--- app/api/elections/models.py
+++ app/api/elections/models.py
@@ -2,11 +2,7 @@ from flask_restplus import Namespace, Resource, fields
 
 from app.api.elections import namespace
 from app.api.restrictions.models import nested_restriction, restriction_response
-<<<<<<< HEAD
-from app.api.options.models import nested_option
-=======
 from app.api.options.models import nested_option, option_response
->>>>>>> 6252f97... changed a response model of the election view.
 
 election = namespace.model('Elections', {
     'name': fields.String(required=True, description='Election name'),
@@ -17,9 +13,6 @@ election_response = namespace.inherit('ElectionResponse', election, {
     'id': fields.Integer(required=True, description='Election id')
 })
 
-<<<<<<< HEAD
-election_full_model = namespace.model('ElectionFull', {
-=======
 
 election_full_response_model = namespace.model('ElectionFull', {
     'election': fields.Nested(election_response),
@@ -28,7 +21,6 @@ election_full_response_model = namespace.model('ElectionFull', {
 })
 
 election_user_response_model = namespace.model('ElectionFullResponse', {
->>>>>>> 6252f97... changed a response model of the election view.
     'election': fields.Nested(election),
     'restrictions': fields.Nested(nested_restriction),
     'options': fields.List(fields.Nested(nested_option)),
@@ -40,4 +32,3 @@ election_full_response_model = namespace.model('ElectionFullResponse', {
     'votes_number': fields.Integer(description='Total vote number'),
     'can_vote': fields.Boolean(description=' If user can vote or revote'),
 })
-
